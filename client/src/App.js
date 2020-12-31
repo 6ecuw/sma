e431236322314ac7191e85a135e2ddb883b0b572
Tsepilov Vladislav (6ecuw) <fickleprogger@gmail.com>
2020-12-31 12:52:50 +0300

    Semantic UI & Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react'
+import './App.css'
+import { MenuBar } from './components'
+import { Home, Login, Register } from './pages'
 
 function App() {
   return (
-    <div>Hi World!</div>
-  );
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/register" component={Register}></Route>
+      </Container>
+    </Router>
+  )
 }
 
-export default App;
+export default App
